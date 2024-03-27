e2fe201f0f547ecdbac95d5eabab00aaef446e74
Таня Дашипилова <tanya.volk01@mail.ru>
2024-03-27 11:38:30 +0800

    3 коммит через терминал Lesson 12

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 console.log(`   Типы данных\n`);
 console.log('JavaScript позволяет узнать тип данных с помощью оператора typeof:');
-typeof 3;      // number
-typeof 'Game'; // string 
-"Типы данных Number, BigInt и String — это примитивные типы."
+console.log(typeof 3);      // number
+console.log(typeof 'Game\n'); // string 
+
+console.log("Типы данных Number, BigInt и String — это примитивные типы.");
+console.log(`  С его помощью составноuj типf Object можно объединять данные 
+разных типов в одно значение, например, мы можем создать пользователя,
+добавив к нему имя и возраст:`);
+const user = { name: 'Toto', age: 33 };
+console.log(user);
