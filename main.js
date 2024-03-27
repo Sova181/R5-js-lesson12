18115180f8aecd288619eea8ecdca3bff98e56ac
Таня Дашипилова <tanya.volk01@mail.ru>
2024-03-27 11:25:36 +0800

    2 коммит через  терминал Lesson12

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log(`   Типы данных`);
+console.log(`   Типы данных\n`);
+console.log('JavaScript позволяет узнать тип данных с помощью оператора typeof:');
+typeof 3;      // number
+typeof 'Game'; // string 
+"Типы данных Number, BigInt и String — это примитивные типы."
